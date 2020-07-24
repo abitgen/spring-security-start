d55c62fe752b698ca44b348687cbeb2ffad9c4dc
abitgen <abitgenius@gmail.com>
2020-07-24 18:07:46 +0530

    Add Spring Security dependency
    Add a new module with Spring security dependency

---
+++ 02.default-security/src/test/kotlin/io/github/abitgen/springsecuritystart/DefaultSecurityApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.abitgen.springsecuritystart
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DefaultSecurityApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
